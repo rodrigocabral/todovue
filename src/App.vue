a4da75f19fe6335033008136ce90d69c339a57db
Rodrigo Cabral <rodrigo.cabral@contassconsultoria.com.br>
2020-07-21 18:43:15 -0300

    Progressbar component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>Tasks</h1>
+    <TaskProgress :progress="progress" />
     <NewTask @taskAdded="addTask" />
     <TaskGrid :tasks="tasks" @taskDeleted="deleteTask" @taskStateChange="toggleTaskState" />
   </div>
@@ -9,15 +10,24 @@
 <script>
 import TaskGrid from "@/components/TaskGrid.vue";
 import NewTask from "@/components/NewTask.vue";
+import TaskProgress from "@/components/TaskProgress.vue";
 export default {
   components: {
     TaskGrid,
-    NewTask
+    NewTask,
+    TaskProgress
   },
   data() {
     return {
       tasks: []
     };
+  },
+  computed: {
+    progress() {
+      const total = this.tasks.length;
+      const done = this.tasks.filter(t => !t.pending).length;
+      return Math.round((done / total) * 100) || 0;
+    }
   },
   methods: {
     addTask(task) {
